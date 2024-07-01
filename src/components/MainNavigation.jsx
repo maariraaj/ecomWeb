4dd32cee7df94af84646937f276d340a915d6738
Mariraj <maari000@hotmail.com>
2024-07-01 19:09:50 +0530

    temp

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -9,11 +9,11 @@ function MainNavigation() {
     const numberOfCartItems = ctx.cartItems.reduce((curNumber, item) => {
         return curNumber + item.quantity;
     }, 0);
-    
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">The Generics</a>
+                <a className="navbar-brand" href="#">The Artistic Aura</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
